0b2bbb2fecc12d0d59259ffc43d219fe2d295be5
gparin <kittikun.real@gmail.com>
2023-08-30 15:36:20 +0700

    refactor(structure):partition client and server

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+    return (<header className="Navbar">Movies review</header>)
+}
+
+export default Navbar;
---
+++ client/src/Post.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+const Post = ({id, title, deletePost}) => {
+
+    return(
+        <div className='Post'>
+            <button className='Delete-post' onClick={()=>deletePost(id)}>delete</button>
+            <div><p>Comment: {id}</p></div>
+            <div className='Title'>{title}</div>
+        </div>
+    )
+}
+
+Post.propTypes = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    deletePost: PropTypes.func.isRequired
+}
+export default Post 
