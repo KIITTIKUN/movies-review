71be841ad87c97aa0677b661a96f556851e20fea
gparin <kittikun.real@gmail.com>
2023-09-04 23:55:15 +0700

    style(post):

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -5,7 +5,7 @@ const Post = ({id, title, deletePost}) => {
     return(
         <div className='Post'>
             <button className='Delete-post' onClick={()=>deletePost(id)}>delete</button>
-            <Link to={`movieReviewDatas/${id}`}>
+            <Link to={`movieReviewDatas/${id}`} style={{ textDecoration: 'none',color: 'rgb(0,0,0)' }}>
             <div><p>Comment: {id}</p></div>
             <div className='Title'>{title}</div>
             </Link>
