d003210174593e92ed23fe701cc81895ccd6cfbd
gparin <kittikun.real@gmail.com>
2023-10-14 15:29:40 +0700

    refactor(auth):

--- client/src/auth/authContext.jsx
+++ client/src/auth/authContext.jsx
@@ -1,44 +1,36 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-export const AuthContext = createContext({ userId: '', auth: false, token: '' });
+export const AuthContext = createContext({ id: '', username: '', auth: false });
 
 export const AuthProvider = ({ children }) => {
-  // Initialize user state based on Local Storage or defaults
   const [user, setUser] = useState(() => {
-    const storedAuth = localStorage.getItem('auth');
-    const initialAuth = storedAuth === 'true' ? true : false;
+    const storedAuth = localStorage.getItem('auth') === 'true';
     const initialId = localStorage.getItem('id') || '';
     const initialUsername = localStorage.getItem('username') || '';
-    return { id: initialId, username: initialUsername, auth: initialAuth };
+    return { id: initialId, username: initialUsername, auth: storedAuth };
   });
 
   useEffect(() => {
     localStorage.setItem('auth', user.auth.toString());
-  }, [user.auth]);
-
-  useEffect(() => {
     localStorage.setItem('id', user.id);
-  }, [user.id]);
-
-  useEffect(() => {
     localStorage.setItem('username', user.username);
-  }, [user.username]);
+  }, [user.auth, user.id, user.username]);
 
-  const login = (id,username) => {
-    setUser((user) => ({
-      id: id,
-      username : username,
+  const login = (id, username) => {
+    setUser({
+      id,
+      username,
       auth: true,
-    }));
+    });
   };
 
   const logout = () => {
-    setUser((user) => ({
+    setUser({
       id: '',
-      username : '',
-      auth: true,
-    }));
+      username: '',
+      auth: false,
+    });
   };
 
   return (
